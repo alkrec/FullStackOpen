5293b1b2e1b949586ecdc193b9bd3cc1b529c936
alkrec <118450684+alkrec@users.noreply.github.com>
2023-04-09 16:49:05 +0300

    completed 2.5

--- Part2/courseinfo/src/App.js
+++ Part2/courseinfo/src/App.js
@@ -1,37 +1,4 @@
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => {
-  const listParts = parts.map((part) => <Part key={part.id} part={part}></Part>)
-  const total = parts.reduce((accumulator, part) => accumulator + part.exercises, 0)
-  //console.log(total)
-  return (
-    <div>
-      {listParts}
-      <p>total of {total} exercises</p>
-    </div>
-  )
-}
-
-
-const Header = (props) => {
-  const { title } = props
-  return (
-    <h2>{title}</h2>
-  )
-}
-
-const Course = (props) => {
-  const { course } = props
-  return (
-    <div>
-      <Header title={course.name}></Header>
-      <Content parts={course.parts}></Content>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
