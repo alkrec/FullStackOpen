f39b37dd9da4c561a4b9d2ef3e83e17cb45d4daf
Alexander Krecouzos <alex.krecouz@gmail.com>
2023-05-22 12:57:54 +0300

    completed 5.3

--- Part5/bloglist/frontend/src/App.js
+++ Part5/bloglist/frontend/src/App.js
@@ -11,6 +11,10 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
@@ -19,9 +23,10 @@ const App = () => {
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedInUser')
-    if(loggedUserJSON) {
+    if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -37,6 +42,7 @@ const App = () => {
         'loggedInUser', JSON.stringify(user)
       )
 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -52,18 +58,36 @@ const App = () => {
     setUser(null);
     window.localStorage.removeItem('loggedInUser')
   }
-  
- 
-  
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const newBlog = { //unnecessary to write for example, title: title in ES6
+      title,
+      author,
+      url
+    }
+
+    const createdBlog = await blogService.create(newBlog)
+    setBlogs(blogs.concat(createdBlog))
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+    console.log(createdBlog)
+  }
+
+
+
   if (user === null) {
     return (
       <div>
-        <Notification message={errorMessage}/>
+        <Notification message={errorMessage} />
         <h2>Log in to application</h2>
         <form onSubmit={handleLogin}>
-        <div>
-          username
-            <input 
+          <div>
+            username
+            <input
               type="text"
               value={username}
               name="Username"
@@ -72,14 +96,14 @@ const App = () => {
           </div>
           <div>
             password
-              <input
-                type="password"
-                value={password}
-                name="Password"
-                onChange={(({ target }) => setPassword(target.value))}  //An object is given to them as a parameter, and they destructure the field target from the object and save its value to the state.
-              />
-        </div>
-        <button type="submit">login</button>
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={(({ target }) => setPassword(target.value))}  //An object is given to them as a parameter, and they destructure the field target from the object and save its value to the state.
+            />
+          </div>
+          <button type="submit">login</button>
         </form>
       </div>
     )
@@ -90,6 +114,33 @@ const App = () => {
       <button onClick={handleLogout}>Logout</button>
       <h2>blogs</h2>
       <p>{user.name} logged in</p>
+
+      <h2>create new</h2>
+      <form onSubmit={handleSubmit}>
+        <p>title:
+          <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={(event) => {setTitle(event.target.value) }} />
+        </p>
+        <p>author:
+          <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={(event) => {setAuthor(event.target.value) }} />
+        </p>
+        <p>
+          url:
+          <input
+            type="text"
+            value={url}
+            name="Url"
+            onChange={(event) => {setUrl(event.target.value) }} />
+        </p>
+        <button type="submit">create</button>
+      </form>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
