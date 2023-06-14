2914e404ec581e057a49e5e29a2502ca9d1c6b5c
Alexander Krecouzos <alex.krecouz@gmail.com>
2023-06-14 15:30:33 +0300

    completed 5.14

--- Part5/bloglist/frontend/src/components/Blog.test.js
+++ Part5/bloglist/frontend/src/components/Blog.test.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-beforeEach(() => {
-
-})
-
-test('renders blog title and author by default', () => {
+describe('<Blog />', () => {
+  let container
   const blog = {
     title: 'blog title',
     author: 'blog author',
@@ -27,11 +25,28 @@ test('renders blog title and author by default', () => {
   const mockDeleteHandler = jest.fn()
   const mockUpdateHandler = jest.fn()
 
-  const container = render(<Blog blog={blog} user={user} deleteBlog={mockDeleteHandler} updateBlog={mockUpdateHandler}/>).container
+  beforeEach(() => {
+    container = render(<Blog blog={blog} user={user} deleteBlog={mockDeleteHandler} updateBlog={mockUpdateHandler}/>).container
+  })
 
-  const divDefault = container.querySelector('.defaultContent')
-  expect(divDefault).toHaveTextContent(blog.title)
-  expect(divDefault).toHaveTextContent(blog.author)
-  expect(divDefault).not.toHaveTextContent(blog.url)
-  expect(divDefault).not.toHaveTextContent(blog.likes)
+  test('renders blog title and author by default', () => {
+    const divDefault = container.querySelector('.defaultContent')
+    expect(divDefault).toHaveTextContent(blog.title)
+    expect(divDefault).toHaveTextContent(blog.author)
+    expect(divDefault).not.toHaveTextContent(blog.url)
+    expect(divDefault).not.toHaveTextContent(blog.likes)
+  })
+
+
+  test('url and likes shown when show button clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+
+    await user.click(button)
+    screen.debug(container)
+    const div = container.querySelector('.togglableContent')
+    expect(div).not.toHaveStyle('display: none')
+    expect(div).toHaveTextContent(blog.url)
+    expect(div).toHaveTextContent(blog.likes)
+  })
 })
