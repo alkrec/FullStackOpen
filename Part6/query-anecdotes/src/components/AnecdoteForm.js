69b28eb1f3a676a25951226d3b0c4f643d78dc30
Alexander Krecouzos <alex.krecouz@gmail.com>
2023-06-21 14:12:09 +0300

    completed 6.20

--- Part6/query-anecdotes/src/components/AnecdoteForm.js
+++ Part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -2,7 +2,7 @@ const AnecdoteForm = () => {
 
   const onCreate = (event) => {
     event.preventDefault()
-    const content = event.target.anecdote.value
+    // const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     console.log('new anecdote')
 }
